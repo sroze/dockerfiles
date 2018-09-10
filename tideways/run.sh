d88a0d3442cd08a0073bee5fee0f400768f999a0
Samuel ROZE <samuel.roze@gmail.com>
2018-09-10 12:05:38 +0200

    Supports the `TIDEWAYS_DAEMON_EXTRA` environment variable

--- tideways/run.sh
+++ tideways/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tideways-daemon --hostname=tideways-daemon --address=0.0.0.0:9135 >> /dev/stdout 2>/dev/stderr
+tideways-daemon --hostname=tideways-daemon --address=0.0.0.0:9135 $TIDEWAYS_DAEMON_EXTRA >> /dev/stdout 2>/dev/stderr
 
 /sbin/my_init
